221dcbecccb9ee5126062c5f6f7739b4c373c486
Nikita Byzov <spamsmails@icloud.com>
2022-11-29 11:09:12 +0600

    Добавлено шифрование базы через связку ключей

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -17,10 +17,63 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // добавляем Firebase
         FirebaseApp.configure()
         
+        func getKey() -> Data {
+            // зададим уникальное имя для связки ключей
+            let keychainIdentifier = "io.Realm.EncryptionExampleKey"
+            let keychainIdentifierData = keychainIdentifier.data(using: String.Encoding.utf8, allowLossyConversion: false)!
+            
+            // есть ли ключ уже в связке ключей
+            var query: [NSString: AnyObject] = [
+                kSecClass: kSecClassKey,
+                kSecAttrApplicationTag: keychainIdentifierData as AnyObject,
+                kSecAttrKeySizeInBits: 512 as AnyObject,
+                kSecReturnData: true as AnyObject
+            ]
+            
+            var dataTypeRef: AnyObject?
+            var status = withUnsafeMutablePointer(to: &dataTypeRef) { SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0)) }
+            if status == errSecSuccess {
+                return dataTypeRef as! Data
+            }
+            
+            var key = Data(count: 64)
+            key.withUnsafeMutableBytes({ (pointer: UnsafeMutableRawBufferPointer) in
+                let result = SecRandomCopyBytes(kSecRandomDefault, 64, pointer.baseAddress!)
+                assert(result == 0, "Failed to get random bytes")
+            })
+            
+            // сохраним ключ в связку ключей
+            query = [
+                kSecClass: kSecClassKey,
+                kSecAttrApplicationTag: keychainIdentifierData as AnyObject,
+                kSecAttrKeySizeInBits: 512 as AnyObject,
+                kSecValueData: key as AnyObject
+            ]
+            
+            status = SecItemAdd(query as CFDictionary, nil)
+            assert(status == errSecSuccess, "Failed to insert the new key in the keychain")
+            
+            return key
+        }
+        
+        var config = Realm.Configuration(encryptionKey: getKey())
+        
         // Realm обновление версии
         // если изменяем какие-то данные в таблице, то тут надо цифру +1 сделать
-        let config = Realm.Configuration(schemaVersion: 1)
+        
+        config.schemaVersion = 1
         Realm.Configuration.defaultConfiguration = config
+        
+        // тут сразу проверим доступность с этим ключом, если нет, то кинем приложение
+        do {
+            let realm = try Realm(configuration: config)
+        } catch let error as NSError {
+            fatalError("Error opening realm: \(error)")
+        }
+        
+        // путь папки на устройстве -
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])
+        
         return true
     }
     
