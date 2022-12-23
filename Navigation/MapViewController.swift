cfbd3ea49c3b936f10aa0d43d60a71c6c650aa91
Nikita Byzov <spamsmails@icloud.com>
2022-12-23 13:45:13 +0600

    правки по итогам КР

--- Navigation/MapViewController.swift
+++ Navigation/MapViewController.swift
@@ -11,6 +11,10 @@ import CoreLocation
 
 class MapViewController: UIViewController {
     
+    var addressOfCity: String?
+    var latitude: CLLocationDegrees?
+    var longitude: CLLocationDegrees?
+    
     private lazy var networkManager = CLLocationManager()
     
     private lazy var mapView: MKMapView = {
@@ -29,7 +33,7 @@ class MapViewController: UIViewController {
         
         NSLayoutConstraint.activate([
             
-            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
@@ -37,65 +41,172 @@ class MapViewController: UIViewController {
         ])
     }
     
+    private func setupNavigationBar(){
+        navigationItem.title = "Маршрут"
+        navigationController?.navigationBar.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        // создаю новый объект в верхнем баре
+        let newTown = UIBarButtonItem(image: UIImage(systemName: "location.magnifyingglass"),
+                                      style: .plain,
+                                      target: self,
+                                      action: #selector(newTownTapped))
+        
+        // добавляю его в доступные к выводу справа и слева
+        navigationItem.rightBarButtonItems = [newTown]
+    }
+    
+    @objc private func newTownTapped(){
+        
+        TextPicker.defaultPicker.getText(in: self) { text in
+            self.addressOfCity = text
+            if self.addressOfCity != "" || self.addressOfCity != nil {
+                self.forwardGeocoding(address: self.addressOfCity!) { data in
+                    self.latitude = data.latitude
+                    self.longitude = data.longitude
+                    
+                    print(self.addressOfCity!)
+                    print(self.latitude!)
+                    print(self.longitude!)
+                    
+                    self.setPinAndMakeRoute(lat: self.latitude!, lot: self.longitude!)
+                } } else {
+                    print("nil field")
+                }
+        }
+    }
+    
+    private func forwardGeocoding(address: String, completion: @escaping ((CLLocationCoordinate2D) -> Void)) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address, completionHandler: { (placemarks, error) in
+            if error != nil {
+                print("Failed to retrieve location")
+                return
+            }
+            
+            var location: CLLocation?
+            
+            if let placemarks = placemarks, placemarks.count > 0 {
+                location = placemarks.first?.location
+            }
+            
+            if let location = location {
+                let coordinate = location.coordinate
+                print("\nlat: \(coordinate.latitude), long: \(coordinate.longitude)")
+                completion(coordinate)
+            }
+            else
+            {
+                print("No Matching Location Found")
+            }
+        })
+    }
+    
+    
     private func requestCurrentUserLocation(){
         networkManager.requestWhenInUseAuthorization()
         networkManager.delegate = self
         networkManager.startUpdatingLocation()
     }
     
-    private func setupPid(){
-        
-        let coordinates: CLLocationCoordinate2D?
-        
-        if networkManager.authorizationStatus.rawValue == 4 {
-            coordinates = CLLocationCoordinate2D(latitude: (networkManager.location?.coordinate.latitude)!, longitude: (networkManager.location?.coordinate.longitude)!)
-        } else {
-            coordinates = CLLocationCoordinate2D(latitude: 55.44, longitude: 37.36)
-        }
-        
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = coordinates!
-        mapView.addAnnotation(annotation)
-        
-        let center = coordinates!
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        let region = MKCoordinateRegion(center: center, span: span)
-        mapView.setRegion(region, animated: true)
-    }
+    //    private func setupPid(){
+    //
+    //        let coordinates: CLLocationCoordinate2D?
+    //
+    //        if networkManager.authorizationStatus.rawValue == 4 {
+    //            coordinates = CLLocationCoordinate2D(latitude: (networkManager.location?.coordinate.latitude)!, longitude: (networkManager.location?.coordinate.longitude)!)
+    //        } else {
+    //            coordinates = CLLocationCoordinate2D(latitude: 55.44, longitude: 37.36)
+    //        }
+    //
+    //        let annotation = MKPointAnnotation()
+    //        annotation.coordinate = coordinates!
+    //        mapView.addAnnotation(annotation)
+    //
+    //        let center = coordinates!
+    //        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    //        let region = MKCoordinateRegion(center: center, span: span)
+    //        mapView.setRegion(region, animated: true)
+    //    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         requestCurrentUserLocation()
         setupView()
-        setupPid()
-        createRoute()
+        setupNavigationBar()
+        //setupPid()
+        //createRoute()
     }
     
-    private func createRoute(){
+    //    private func createRoute(){
+    //        if networkManager.authorizationStatus.rawValue == 4 {
+    //            let begginDote = MKPlacemark(coordinate: networkManager.location!.coordinate)
+    //            let distinationDote = MKPlacemark(coordinate: CLLocationCoordinate2D(
+    //                latitude: (networkManager.location?.coordinate.latitude)! + 0.5,
+    //                longitude: (networkManager.location?.coordinate.longitude)! + 0.5))
+    //            let request = MKDirections.Request()
+    //            request.source = MKMapItem(placemark: begginDote)
+    //            request.destination = MKMapItem(placemark: distinationDote)
+    //            request.transportType = .any
+    //
+    //            let direction = MKDirections(request: request)
+    //            direction.calculate { response, error in
+    //                if error == nil {
+    //                    let rout = response!.routes[0]
+    //                    self.mapView.addOverlay(rout.polyline, level: .aboveRoads)
+    //                    self.mapView.delegate = self
+    //                    let rect = rout.polyline.boundingMapRect
+    //                    self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+    //                } else { print(error!.localizedDescription) }
+    //            }
+    //
+    //        } else {
+    //            print("Не могу создать, нет разрешения")
+    //        }
+    //
+    //    }
+    
+    private func setPinAndMakeRoute(lat: CLLocationDegrees, lot: CLLocationDegrees){
+        let coordinates: CLLocationCoordinate2D?
+        let begginDote: MKPlacemark?
+        let distinationDote: MKPlacemark?
+        let request = MKDirections.Request()
+        let direction: MKDirections?
+        
         if networkManager.authorizationStatus.rawValue == 4 {
-            let begginDote = MKPlacemark(coordinate: networkManager.location!.coordinate)
-            let distinationDote = MKPlacemark(coordinate: CLLocationCoordinate2D(
-                latitude: (networkManager.location?.coordinate.latitude)! + 0.5,
-                longitude: (networkManager.location?.coordinate.longitude)! + 0.5))
-            let request = MKDirections.Request()
-            request.source = MKMapItem(placemark: begginDote)
-            request.destination = MKMapItem(placemark: distinationDote)
+            coordinates = CLLocationCoordinate2D(latitude: lat, longitude: lot)
+            begginDote = MKPlacemark(coordinate: coordinates!)
+            distinationDote = MKPlacemark(coordinate: CLLocationCoordinate2D(
+                latitude: lat + 0.5,
+                longitude: lot + 0.5))
+            request.source = MKMapItem(placemark: begginDote!)
+            request.destination = MKMapItem(placemark: distinationDote!)
             request.transportType = .any
             
-            let direction = MKDirections(request: request)
-            direction.calculate { response, error in
+            direction = MKDirections(request: request)
+            direction!.calculate { response, error in
                 if error == nil {
-                    let rout = response?.routes[0]
-                    self.mapView.addOverlay(rout!.polyline, level: .aboveRoads)
+                    let rout = response!.routes[0]
+                    self.mapView.addOverlay(rout.polyline, level: .aboveRoads)
                     self.mapView.delegate = self
+                    let rect = rout.polyline.boundingMapRect
+                    self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
                 } else { print(error!.localizedDescription) }
             }
             
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinates!
+            mapView.addAnnotation(annotation)
+            
+            let center = coordinates!
+            let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            let region = MKCoordinateRegion(center: center, span: span)
+            mapView.setRegion(region, animated: true)
+            
         } else {
-            print("Не могу создать, нет разрешения")
+            print("Нет разрешения, ничего не делай")
         }
-        
     }
 }
 
